041b841056e564ce43812522beeb4e2959ebb985
Hunzila Bin Naseem <hunzilabinaseem456@gmail.com>
2024-03-22 16:37:30 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+   
+    document.getElementById('searchInputTable').addEventListener('input', searchTable);
+  
+    function searchTable() {
+      
+      var searchText = this.value.toLowerCase();
+      console.log(searchText);
+  
+      var rows = document.querySelectorAll('#tabledata tbody tr');
+  
+      rows.forEach(function(row) {
+        var cells = row.querySelectorAll('td');
+        var found = false;
+  
+        cells.forEach(function(cell) {
+          if (cell.textContent.toLowerCase().includes(searchText)) {
+            found = true;
+          }
+        });
+  
+        if (found) {
+          row.style.display = '';
+        } else {
+          row.style.display = 'none';
+        }
+      });
+    }
+  });
+  
+  fetch('https://corsproxy.io/?http%3A%2F%2Ftest.techsy.pk%2FTemp%2Fgettestingdata')
+    .then((data) => data.json())
+    .then((objectData) => {
+      console.log(objectData.Data);
+      let myObjectData = objectData.Data;
+      let tableData = "";
+      myObjectData.forEach((values) => {
+        tableData += `<tr>    
+           <td>${values.id}</td>
+           <td>${values.name}</td>
+           <td>${values.Class}</td>
+           <td>${values.admissiondts}</td>
+           <td>${values.status}</td>
+         </tr>`;
+      });
+      document.getElementById("table_body").innerHTML = tableData;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  
